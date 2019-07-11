32172e86b08e924e62b48c4f95e9ad495fb45146
louxiaojian <louxiaojian@gmail.com>
2019-07-11 20:18:23 +0800

    @add assets files and update index.js

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 'use strict';
 
 module.exports = {
+  website: {
+    assets: './assets_diy',
+    css: [
+      'diy.css'
+    ]
+  },
   hooks: {
     init: function init() {
-      var _this$config$get = this.config.get('pluginsConfig.chart')
+      var _this$config$get = this.config.get('pluginsConfig.diy')
     },
     "page:before": function pageBefore(page) {
 
-	  console.log(page.content)
+      page.content = page.content.replace(/([\n\s]*)\{([^}]+)\}\(([^)]+)\)(\s*(?:$|\n+))/g, function (mats, beginCharacter, type, url, endCharacter) {
+        let width = "100%"
+        let height = "500"
+        if (type === "video") {
+          width = "640"
+          height = "264"
+        }
+        let typesMatchs = {
+          'iframe': `<iframe id="iframe" width="${width}" height="${height}" allowTransparency="true" src="${url}" frameborder="0" marginheight="0" marginwidth="0"></iframe>`,
+          "video": `<video width="${width}" height="${height}" controls="controls" src="${url}" preload="preload"></video>`,
+          "audio": `<audio controls src="${url}"></audio>`
+        }
 
-		page.content = page.content.replace(/([\n\s]*)\{([^}]+)\}\(([^)]+)\)(\s*(?:$|\n+))/g,function(mats,beginCharacter,type,url,endCharacter){
-			console.log(mats,type,url)
-			if(type === 'iframe'){
-				return `${beginCharacter}<iframe id="iframe" width="100%" height="500" allowTransparency="true" src="${url}" frameborder="0" marginheight="0" marginwidth="0"></iframe>${endCharacter}`
-			}
-			return mats
-		})
+        return typesMatchs[type] ? `${beginCharacter + typesMatchs[type] + endCharacter}` : mats
+
+      })
+
       return page;
-	  
+
     }
   }
 };
